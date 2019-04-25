9c125a123b70ef9a5e0bee6214f50a8538e903cc
HARSH <iamhp2k@gmail.com>
2019-04-25 13:40:10 +0530

    Removed Device Only Environment Setup

--- environment.sh
+++ environment.sh
@@ -5,7 +5,6 @@ sudo apt-get update
 sudo apt-get install -y bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev unzip openjdk-8-jdk python ccache libtinfo5
 sudo apt-get upgrade -y
 
-
 echo "================== INSTALLING GIT-REPO ========================== "
 wget https://storage.googleapis.com/git-repo-downloads/repo
 chmod a+x repo
@@ -14,12 +13,3 @@ sudo install repo /usr/local/bin/repo
 echo -e "\n================== CONFIGURING GIT ==================\n"
 git config --global user.email "iamhp2k@gmail.com"
 git config --global user.name "hpnightowl"
-
-echo "======================KERNEL============================="
-git clone https://github.com/hpnightowl/android_kernel.git -b pie kernel/cyanogen/msm8916
-
-echo "======================Device Tree========================"
-git clone https://github.com/hpnightowl/android_device_yu_jalebi.git -b pie device/yu/jalebi
-
-echo "=====================VeNDOR=============================="
-git clone https://github.com/hpnightowl/proprietary_vendor_yu.git -b pie vendor/yu
