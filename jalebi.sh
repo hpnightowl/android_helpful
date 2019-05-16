4327730d2d8bce3269bc9bbddfa7300c376b9ebe
HARSH <48650798+hpnightowl@users.noreply.github.com>
2019-05-16 16:17:01 +0530

    Create jalebi.sh

---
+++ jalebi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "======================= Jalebi Repo's ======================================"
+
+git clone https://github.com/hpnightowl/android_device_yu_jalebi.git device/yu/jalebi
+git clone https://github.com/hpnightowl/android_kernel_cyanogen_msm8916.git kernel/cyanogen/msm8916
+git clone https://github.com/hpnightowl/vendor_yu.git vendor/yu
