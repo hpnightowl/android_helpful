aa48294069c94a8b3738e0724c0a56781c4fa0cf
HARSH <iamhp2k@gmail.com>
2020-11-04 15:35:02 +0530

    Create ginkgo.sh

---
+++ ginkgo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/hpnightowl/android_vendor_xiaomi_ginkgo.git vendor/xiaomi/ginkgo
+git clone https://github.com/hpnightowl/android_device_xiaomi_ginkgo.git device/xiaomi/ginkgo
+git clone https://github.com/hpnightowl/android_device_xiaomi_trinket-common.git device/xiaomi/trinket-common
+git clone https://github.com/hpnightowl/android_kernel_xiaomi_ginkgo-owl.git kernel/xiaomi/ginkgo
