61c6bf48a74baf345305e6af850cca2da41d5da9
HARSH <iamhp2k@gmail.com>
2020-11-25 13:53:49 +0530

    ginkgo: clone hals,more vendor and remove kernel as of now :|

--- ginkgo.sh
+++ ginkgo.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-git clone https://github.com/hpnightowl/android_vendor_xiaomi_ginkgo.git vendor/xiaomi/ginkgo
 git clone https://github.com/hpnightowl/android_device_xiaomi_ginkgo.git device/xiaomi/ginkgo
+git clone https://github.com/hpnightowl/android_vendor_xiaomi_trinket-common.git vendor/xiaomi/trinket-common
 git clone https://github.com/hpnightowl/android_device_xiaomi_trinket-common.git device/xiaomi/trinket-common
-git clone https://github.com/hpnightowl/android_kernel_xiaomi_ginkgo-owl.git kernel/xiaomi/ginkgo
+git clone https://github.com/hpnightowl/android_vendor_xiaomi_ginkgo.git vendor/xiaomi/ginkgo
+rm -rf hardware/qcom-caf/sm8150/display
+rm -rf hardware/qcom-caf/sm8150/audio
+rm -rf hardware/qcom-caf/sm8150/media
+git clone https://github.com/hpnightowl/hardware_qcom-caf_display-sm8150.git hardware/qcom-caf/sm8150/display
+git clone https://github.com/owl-aosp/hardware_qcom-caf_sm8150_audio.git hardware/qcom-caf/sm8150/audio
+git clone https://github.com/owl-aosp/hardware_qcom-caf_sm8150_media.git hardware/qcom-caf/sm8150/media
