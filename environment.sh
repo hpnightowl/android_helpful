d857a0a6b761e48573ddf98d5a2f87596a5d9caf
Harsh <iamhp2k@gmail.com>
2021-07-24 00:19:56 +0530

    script: environment: add libssl-dev

--- environment.sh
+++ environment.sh
@@ -2,7 +2,7 @@
 
 echo "====================Build Environment Rom========================"
 sudo apt-get update
-sudo apt-get install -y bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev unzip openjdk-8-jdk python ccache libtinfo5
+sudo apt-get install -y bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev unzip openjdk-8-jdk libssl-dev python ccache libtinfo5
 sudo apt-get upgrade -y
 
 echo "================== INSTALLING GIT-REPO ========================== "
